090bae829491b39cbf9b6cc33ac773780fdc1038
xhlgr <1850125838@qq.com>
2021-05-20 21:21:25 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'ur912507-8258182cfa5e7c2c21902bb9'
+	'm786534547-eb3278191d10ef187b79bc12','m786423362-aed55ab0b9c708d264d57544','m786125750-a62f7c3a2b0c34b934204676','m786596730-6fc83292c2ea8df493877046','m786125754-5f2e7df8bcf96fdd706d4643','m786125756-2eb58c7d022102a40e191cbd','m786596733-bb1c3c1cefec5ebe9c64fd76'
 ];
 
 // refresh interval (in seconds)
